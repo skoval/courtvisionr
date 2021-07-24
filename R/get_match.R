e9f4d7ec2949e1a3598e5505fa8b0543100ee7fd
skoval <skovalchik@gmail.com>
2021-07-24 15:37:16 +1000

    Added README

--- R/get_match.R
+++ R/get_match.R
@@ -8,10 +8,7 @@
 #' get_match(2021, "MS701", "ao")
 get_match <- function(year, matchid, event = c("ao", "rg")){
 	
-	if(year < 2021 & event == "rg")
-		url <- glue::glue("https://itp.infosys-platforms.com/api/rg/court-vision/year/{year}/eventId/520/matchId/{matchid}/pointId/0_0_0")
-	else 
-		if(event == "rg")
+	if(event == "rg")
 		url <- glue::glue("https://itp-rg-sls.infosys-platforms.com/prod/api/court-vision/year/{year}/eventId/520/matchId/{matchid}/pointId/0_0_0")
 		else
 		url <- glue::glue("https://itp-ao.infosys-platforms.com/api/court-vision/year/{year}/eventId/580/matchId/{matchid}/pointId/0_0_0")
--- R/get_points.R
+++ R/get_points.R
@@ -2,8 +2,11 @@
 #' @param x List from JSON input, return from \code{get_match}
 #' @param matchid character matchid as on grand slam results page (e.g. 'MS001')
 #' @param year Year of match (yyyy)
+#' @param event Character name of event
 #' @export
-get_points <- function(x, matchid = NULL, year = NULL){
+#' @examples
+#' get_points(get_match(2021, "MS701", "ao"), "MS701", 2021)
+get_points <- function(x, matchid = NULL, year = NULL, event = NULL){
 	
 	points <- lapply(x$pointsData, get_trajectory)
 	
@@ -25,6 +28,7 @@ get_points <- function(x, matchid = NULL, year = NULL){
 	
 	points <- points[, matchid := matchid]
 	points <- points[, year := year]
+	points <- points[, event := event]	
 	
 return(points)
 }
